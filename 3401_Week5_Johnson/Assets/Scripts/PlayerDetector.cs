07fa052d4086ab811a189789e1f36653daf7dcc8
aj418014 <47157670+aj418014@users.noreply.github.com>
2019-02-19 13:22:30 -0500

    Added Completed Unity Project
    Added completed Unity Project

---
+++ 3401_Week5_Johnson/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPosition = transform.position;
+        Ray targetDirection = new Ray(playerPosition, transform.TransformDirection(Vector3.forward));
+        if (Physics.Raycast(targetDirection, 5))
+            Debug.Log("There is something here");
+        else
+            Debug.Log("There is nothing in front of me");
+
+    }
+}
